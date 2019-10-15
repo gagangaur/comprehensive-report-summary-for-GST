758eb12ca485c55adb38c695b328f6b7fd15a8f4
gagangaur <gagan1999gaur@gmail.com>
2019-10-15 23:46:28 +0530

    code reviewed

--- gstcal.py
+++ gstcal.py
@@ -4,29 +4,36 @@ wb = xlrd.open_workbook(location)
 sheet = wb.sheet_by_index(0)
 report,temp,final=[],[],[]
 #print(sheet.nrows)
-for i in range(sheet.nrows):
-# for i in range(20):
-    if sheet.cell(i,7).value == "Delivered":
-        temp.append(sheet.cell(i,46).value)
-        temp.append(sheet.cell(i,20).value)
-        temp.append(sheet.cell(i,18).value)
-        temp.append(sheet.cell(i,16).value)
-        temp.append(sheet.cell(i,12).value)
-        temp.append(sheet.cell(i,8).value)
+
+for row in range(sheet.nrows):
+# for i in range(10):
+    if sheet.cell(row,7).value == "Delivered":
+        temp.append(sheet.cell(row,46).value)
+        temp.append(sheet.cell(row,20).value)
+        temp.append(sheet.cell(row,18).value)
+        temp.append(sheet.cell(row,16).value)
+        temp.append(sheet.cell(row,12).value)
+        temp.append(sheet.cell(row,8).value)
         final.append(temp)
         #print(temp)
-        temp=[] 
+        temp=[]
+
 # print(final)
+
 states=set()
 selling_price,sgst,cgst,igst,seller_invoice_amount=0,0,0,0,0
+
 for i in final:
     states.add(i[0])
-#print(*final,sep="\n")
+
 states=list(states)
 states.sort()
 #print(states)
+
+
 for i in states:
     target=""
+    x=0
     selling_price,sgst,cgst,igst,seller_invoice_amount=0,0,0,0,0
     for j in final:
         if i==j[0]:
@@ -50,8 +57,9 @@ for i in states:
                 selling_price+=0
             else:
                 selling_price+=float(j[5])
+
     target=str(i)+" "+"IGST: "+str(igst)+" "+"CGST: "+str(cgst)+" "+"SGST: "+str(sgst)+" "+"seller invoice amount: "+str(seller_invoice_amount)+" "+"Selling Price: "+str(selling_price)
+
     report.append(target)
     #print(target)
-for i in report:
-    print(i)
+print(*report,sep="\n")
